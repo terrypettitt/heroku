3f62243e5e5ca758e27fdff24c0d369c49a6211a
terrypettitt <30344729+terrypettitt@users.noreply.github.com>
2024-06-19 12:23:00 +0100

    Update StartAction.php

--- src/Action/Archive/StartAction.php
+++ src/Action/Archive/StartAction.php
@@ -1,15 +1,5 @@
 <?php
 
-
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS, post, get');
-header('Access-Control-Max-Age: 3600');
-header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
-header('Access-Control-Allow-Credentials: true');
-    
-
-
-
 namespace OTHelloWorld\Action\Archive;
 
 use OpenTok\OpenTok;
